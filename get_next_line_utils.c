8af7105942092cba18ff35c20c93afc1e9c3a4ed
Tansel Bayraktaroglu <tbayrakt@z3t2c2.42bangkok.com>
2024-04-07 14:11:14 +0700

    create get_next_line_utils.c file and add some helper functions

---
+++ get_next_line_utils.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbayrakt <tbayrakt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/07 14:00:32 by tbayrakt          #+#    #+#             */
+/*   Updated: 2024/04/07 14:08:00 by tbayrakt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *str)
+{
+	char	*new_str;
+	int		i;
+
+	i = 0;
+	new_str = malloc(ft_strlen(str) + 1);
+	while (str[i])
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	new_str[i] = 0;
+	return (new_str);
+}
