2e7d4a2635da51fc629640dbeae46b2b9ab87426
Tansel Bayraktaroglu <tbayrakt@z3t2c2.42bangkok.com>
2024-04-10 11:50:14 +0700

    add ft_strjoin function to utils

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,11 +6,11 @@
 /*   By: tbayrakt <tbayrakt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 14:00:32 by tbayrakt          #+#    #+#             */
-/*   Updated: 2024/04/07 14:08:00 by tbayrakt         ###   ########.fr       */
+/*   Updated: 2024/04/10 11:47:51 by tbayrakt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strlen(char *str)
+char	ft_strlen(char *str)
 {
 	int	i;
 
@@ -33,5 +33,31 @@ char	*ft_strdup(char *str)
 		i++;
 	}
 	new_str[i] = 0;
+	return (new_str);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*new_str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	new_str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1)
+			* sizeof(char));
+	if (!new_str)
+		return (0);
+	while (s1[i])
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		new_str[i + j] = s2[j];
+		j++;
+	}
+	new_str[i + j] = 0;
 	return (new_str);
 }
